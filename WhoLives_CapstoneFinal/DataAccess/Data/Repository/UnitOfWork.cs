aaf13024cc6b532f85ac4805ac410beb91ffdeca
Isiah Valdez <isiahvaldez@mail.weber.edu>
2020-03-13 18:37:45 -0600

    Vendor class and repositories, added Vendor to UnitOfWork, ApplicationDbContext

--- WhoLives_CapstoneFinal/DataAccess/Data/Repository/UnitOfWork.cs
+++ WhoLives_CapstoneFinal/DataAccess/Data/Repository/UnitOfWork.cs
@@ -11,10 +11,13 @@ namespace WhoLives.DataAccess.Data.Repository
         private readonly ApplicationDbContext _db;
 
         public IMeasuresRepository Measures { get; private set; } 
+        public IVendorRepository Vendor { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
-            Measures = new MeasuresRepository(_db); 
+            Measures = new MeasuresRepository(_db);
+            Vendor = new VendorRepository(_db);
         }
 
         public void Dispose()
---
+++ WhoLives_CapstoneFinal/WhoLives.Models/Vendor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WhoLives.Models
+{
+    public class Vendor
+    {
+        [Key]
+        public int VendorID { get; set; }
+        [Display(Name = "Name")]
+        public string VendorName { get; set; }
+        [Display(Name = "Website")]
+        public string VendorWebsite { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public string LastModifiedBy { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime LastModifiedDate { get; set; }
+
+        //public ICollection<PurchaseOrder> PurchaseOrders { get; set; } // probably belongs in a View Model
+    }
+}
