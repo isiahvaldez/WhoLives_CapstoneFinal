696b0b041b175d747e07c43ab567cf19cfa2c95e
johnwyke <johnwyke@mail.weber.edu>
2020-03-18 15:01:05 -0600

    created vendor items and edited vendor model

---
+++ WhoLives_CapstoneFinal/WhoLives.Models/VendorItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WhoLives.Models
+{
+    public class VendorItems
+    {
+
+        public int InventoryItemsId { get; set; }
+        public int VendorId { get; set; }
+
+       
+        public InventoryItems InventoryItems { get; set; }
+     
+        public Vendor Vendor { get; set; }
+    }
+}
