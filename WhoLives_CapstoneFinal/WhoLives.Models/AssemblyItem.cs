42426ed430ed8472e2401abefcd92cda1c618038
Isiah Valdez <isiahvaldez@mail.weber.edu>
2020-03-19 07:58:53 -0600

    Added AssemblyItem and InventoryItem attributes to AssemblyItem model

--- WhoLives_CapstoneFinal/WhoLives.Models/AssemblyItem.cs
+++ WhoLives_CapstoneFinal/WhoLives.Models/AssemblyItem.cs
@@ -13,14 +13,17 @@ namespace WhoLives.Models
     {
         [Key]
         public  int AssemblyItemID { get; set; }
+        
+        [ForeignKey("Assembly")]
+        public int AssemblyID { get; set; }
         /// <summary>
         /// The assembly this item belongs to
         /// </summary>
-        [ForeignKey("AssemblyItem")]
-        public int AssemblyID { get; set; }
+        public InventoryItem Assembly { get; set; }
         /// <summary>
         /// The item used in the assembly
         /// </summary>
+        public InventoryItem Item { get; set; }
         [ForeignKey("Item")]
         public int ItemID { get; set; }
         /// <summary>
