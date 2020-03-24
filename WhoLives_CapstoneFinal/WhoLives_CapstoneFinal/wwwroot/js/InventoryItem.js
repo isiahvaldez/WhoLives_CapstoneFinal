03a834fecf4058f23961832b7d8b780a527a0ffa
johnwyke <johnwyke@mail.weber.edu>
2020-03-24 10:30:58 -0600

    Added InventoryItems Javascript

---
+++ WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/wwwroot/js/InventoryItem.js
@@ -0,0 +1,20 @@
+﻿var dataTable; 
+$(document).ready(function () {
+    loadInventoryList();
+});
+
+function loadInventoryList() {
+    dataTable = $('#AllItem').dataTable({
+        "ajax": {
+            "url": "/api/inventoryItem",
+            "type": "GET",
+            "datatype": "json"
+        }
+        "columns": [
+            {
+            "data":"Name"}
+        ]
+
+        
+    });
+}
