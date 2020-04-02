7dcfaf394921dfa08620ee474f6a740e5e280308
theduke55 <40616435+theduke55@users.noreply.github.com>
2020-04-01 20:59:35 -0600

    Built InventoryItemVM

---
+++ WhoLives_CapstoneFinal/WhoLives.Models/ViewModels/InventoryItemVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoLives.Models.ViewModels
+{
+    public class InventoryItemVM
+    {
+        public IEnumerable<SelectListItem> ItemList { get; set; }
+        public IEnumerable<SelectListItem> VendorList { get; set; }
+        public PurchaseOrder OrderInfo { get; set; }
+        public Assembly AssemblyInfo { get; set; }
+        public BuildAssembly BuildInfo { get; set; }
+    }
+}
