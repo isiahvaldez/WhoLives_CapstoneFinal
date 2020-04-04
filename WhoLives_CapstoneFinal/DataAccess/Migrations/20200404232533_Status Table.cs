9b255e17b14c69e2ee575d2f012135064ef49f65
epossatyr <a_cothran@outlook.com>
2020-04-04 17:26:37 -0600

    Migration file

---
+++ WhoLives_CapstoneFinal/DataAccess/Migrations/20200404232533_Status Table.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhoLives.DataAccess.Migrations
+{
+    public partial class StatusTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PurchaseOrders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusID",
+                table: "PurchaseOrders",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrders_StatusID",
+                table: "PurchaseOrders",
+                column: "StatusID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrders_Statuses_StatusID",
+                table: "PurchaseOrders",
+                column: "StatusID",
+                principalTable: "Statuses",
+                principalColumn: "StatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrders_Statuses_StatusID",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseOrders_StatusID",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "StatusID",
+                table: "PurchaseOrders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "PurchaseOrders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
