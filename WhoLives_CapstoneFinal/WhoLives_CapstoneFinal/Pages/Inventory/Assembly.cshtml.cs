13ac108bb8a39537263e2dc99794d4a12e6dc477
theduke55 <40616435+theduke55@users.noreply.github.com>
2020-04-04 15:52:10 -0600

    Tried a new button layout.

---
+++ WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Pages/Inventory/Assembly.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WhoLives_CapstoneFinal
+{
+    public class AssemblyModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
