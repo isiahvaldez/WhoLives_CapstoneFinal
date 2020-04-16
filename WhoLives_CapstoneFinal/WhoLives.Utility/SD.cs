c93ce9e507f66bece456b2b5d7837e246cde98be
John Wyke <wykejohn@gmail.com>
2020-04-15 22:23:56 -0600

    Added Email user can Login. Trying to change from email to username.

---
+++ WhoLives_CapstoneFinal/WhoLives.Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoLives.Utility
+{
+    public class SD
+    {
+        public const string AdminRole = "Admin";
+    }
+}
