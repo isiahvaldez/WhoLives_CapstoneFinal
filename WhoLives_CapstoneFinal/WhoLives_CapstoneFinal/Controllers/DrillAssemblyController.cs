0dd2c2bbab17dbcd018ef7c8dd49c70857b95330
Isiah Valdez <isiahvaldez@mail.weber.edu>
2020-04-16 23:28:20 -0600

    Counting total item qty should be working

--- WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Controllers/DrillAssemblyController.cs
+++ WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Controllers/DrillAssemblyController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WhoLives.DataAccess.Data.Repository.IRepository;
 using WhoLives.Models;
 using WhoLives.Models.ViewModels;
@@ -14,6 +19,16 @@ namespace WhoLives_CapstoneFinal.Controllers
     {
         public List<InventoryItem> itemList;
         public List<int> requiredQty;
+        public List<int> totalQty;
+    }
+
+    public class ItemWithCounts
+    {
+        public int id;
+        public string name;
+        public int looseQty;
+        public int requiredQty;
+        public int totalQty;
     }
 
     [Route("api/[controller]")]
@@ -25,6 +40,7 @@ namespace WhoLives_CapstoneFinal.Controllers
         InventoryItem drill; 
         int drillID = 104; // this is hardcoded and will need to be changed if the item is deleted and readded - IV 4/15/2020
         ItemsRequired itemsRequired = new ItemsRequired();
+        List<ItemWithCounts> itemList = new List<ItemWithCounts>();
 
         [BindProperty]
         public BuildAssemblyVM buildAssemblyVM { get; set; }
@@ -35,7 +51,7 @@ namespace WhoLives_CapstoneFinal.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get(string input)
+        public IActionResult Get()
         {
             drill = _unitOfWork.InventoryItems.GetFirstOrDefault(i => i.InventoryItemID == drillID);
 
@@ -53,22 +69,86 @@ namespace WhoLives_CapstoneFinal.Controllers
             itemsRequired = new ItemsRequired()
             {
                 itemList = _unitOfWork.InventoryItems.GetAll().Where(i => i.IsAssembly != true).ToList(),
-                requiredQty = new List<int>()
+                requiredQty = new List<int>(),
+                totalQty = new List<int>()
             };
 
             foreach (var item in itemsRequired.itemList)
             {
                 itemsRequired.requiredQty.Add(0);
+                itemsRequired.totalQty.Add(item.TotalLooseQty);
             }
-            CountChildItems(drillID, 1);
+            CountAssemblyComponents(drillID, 1); // this is counting the items required for the drill per its recipe
+
+            foreach(var item in buildAssemblyVM.InventoryItems)
+            {
+                if(item.IsAssembly)
+                {
+                    CountChildItems(item.InventoryItemID, item.TotalLooseQty);
+                }
+            }
+
+            // convert list to an object better structured for JSON
+
+            for(int i = 0; i < itemsRequired.itemList.Count; i++)
+            {
+                itemList.Add(new ItemWithCounts()
+                {
+                    id = itemsRequired.itemList[i].InventoryItemID,
+                    name = itemsRequired.itemList[i].Name,
+                    looseQty = itemsRequired.itemList[i].TotalLooseQty,
+                    requiredQty = itemsRequired.requiredQty[i],
+                    totalQty = itemsRequired.totalQty[i]
+                });
+            }
 
             // TODO: change this to give the info we need - IV 4/15/2020
-            return Json(new { data = _unitOfWork.InventoryItems.GetAll().Where(i => i.IsAssembly != true) });
+            string json = JsonConvert.SerializeObject(itemList);
+            var result = JsonConvert.DeserializeObject(json);
+
+            return Content(json);
+            //return Json(new { data = _unitOfWork.InventoryItems.GetAll().Where(i => i.IsAssembly != true) });
 
         }
 
-        // this is used for finding the required number of items to build the drill
-        private void CountChildItems(int itemID, int requiredQty)
+        /// <summary>
+        /// Counts the items needed for an assembly, adds results to itemsRequired.requiredQty
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="requiredQty"></param>
+        private void CountAssemblyComponents(int itemID, int requiredQty)
+        {
+            // build list for this item
+            List<BuildAssembly> buildAssemblies =
+                _unitOfWork.BuildAssemblies.GetAll().Where(i => i.InventoryItemID == itemID).ToList();
+            // get assemblies these builds reference
+            List<Assembly> assemblyList = new List<Assembly>();
+            foreach (var build in buildAssemblies)
+            {
+                assemblyList.Add(_unitOfWork.Assemblies.GetFirstOrDefault(i => i.AssemblyID == build.AssemblyID));
+            }
+            // go through each assembly
+            foreach (var assembly in assemblyList)
+            {
+                InventoryItem currItem = buildAssemblyVM.InventoryItems.ToList().Find(i => i.InventoryItemID == assembly.InventoryItemID);
+                if (currItem.IsAssembly)
+                {
+                    CountAssemblyComponents(currItem.InventoryItemID, assembly.ItemQty);
+                }
+                else
+                {
+                    int index = itemsRequired.itemList.FindIndex(i => i.InventoryItemID == assembly.InventoryItemID);
+                    itemsRequired.requiredQty[index] += requiredQty * assembly.ItemQty;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Counts the items needed for an assembly, adds the results tp itemsRequired.totalQty
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="itemQty"></param>
+        private void CountChildItems(int itemID, int itemQty)
         {
             // build list for this item
             List<BuildAssembly> buildAssemblies =
@@ -90,7 +170,7 @@ namespace WhoLives_CapstoneFinal.Controllers
                 else
                 {
                     int index = itemsRequired.itemList.FindIndex(i => i.InventoryItemID == assembly.InventoryItemID);
-                    itemsRequired.requiredQty[index] += requiredQty * assembly.ItemQty;
+                    itemsRequired.totalQty[index] += itemQty * assembly.ItemQty;
                 }
             }
         }
