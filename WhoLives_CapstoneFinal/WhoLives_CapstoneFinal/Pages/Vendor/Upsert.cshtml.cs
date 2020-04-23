870658c5b1d6ca3e3d245b775040c947c6c96a69
Isiah Valdez <isiahvaldez@mail.weber.edu>
2020-04-23 13:44:26 -0600

    Fixed login redirect

--- WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Pages/Vendor/Upsert.cshtml.cs
+++ WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Pages/Vendor/Upsert.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WhoLives.DataAccess.Data.Repository.IRepository;
@@ -9,6 +10,7 @@ using WhoLives.Models;
 
 namespace WhoLives_CapstoneFinal.Pages.Vendor
 {
+    [Authorize]
     public class UpsertModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
--- WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Startup.cs
+++ WhoLives_CapstoneFinal/WhoLives_CapstoneFinal/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,6 +39,15 @@ namespace WhoLives_CapstoneFinal
                 .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
             
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
